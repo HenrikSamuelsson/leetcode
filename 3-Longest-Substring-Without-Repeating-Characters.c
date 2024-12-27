704945972dfcc62d7318c44d7d0e552771bab4ab
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-12-27 19:35:05 +0100

    [3] [Time Beats: 55.90%] [Memory Beats: 47.92%] - LeetPush

---
+++ 3-Longest-Substring-Without-Repeating-Characters.c
@@ -0,0 +1,34 @@
+// A solution that employs the sliding window technique along with an array that
+// functions as a hash table to keep track of characters and their indices.
+
+#define ALPHABET_SIZE 256
+
+int lengthOfLongestSubstring(char *s) {
+    int charIndex[ALPHABET_SIZE];
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        charIndex[i] = -1; // Initialize to -1 for all ASCII characters.
+    }
+
+    int maxLength = 0; // Length of longest substring found.
+    int startIdx = 0; // Start index of the current substring.
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char currentChar = s[i];
+
+        // If the character is found in the substring, move the start index.
+        if (charIndex[currentChar] >= startIdx) {
+            startIdx = charIndex[currentChar] + 1;
+        }
+
+        // Update the latest index of the character.
+        charIndex[currentChar] = i;
+
+        // Update the maximum length of the substring.
+        int substringLength = i - startIdx + 1;
+        if (substringLength > maxLength) {
+            maxLength = substringLength;
+        }
+    }
+
+    return maxLength;
+}
